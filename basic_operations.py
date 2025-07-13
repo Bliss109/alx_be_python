978de8fb84fce3270828c9099115a5a1c4d17271
Bliss109 <cindy.ogutu@strathmore.edu>
2025-07-13 17:50:12 +0300

    Basic Arithmetic Operations with Python

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# Basic Arithmetic Exercise via ALX
+number1 = 10
+number2 = 5
+
+# Addition of two numbers
+sum = number1 + number2
+difference = number1 - number2
+multiply = number1 * number2
+
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2)+ " is " + str(difference))
+print("Multiplication of " + str(number1) + " and " + str(number2)+ " is " + str(multiply))
+
+
