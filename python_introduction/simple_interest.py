4c523352b71ff1c0143c7b93b85fd530a1f5daf4
Bliss109 <cindy.ogutu@strathmore.edu>
2025-07-13 18:00:33 +0300

    Applying basic arithmetic operations to examples

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#Simple Interest Calculator
+
+#Principal represents value, rate represents annual interest rate and time represents 3 years
+principal = 1000
+rate = 0.05
+time = 3
+
+#Formula for simple interest
+interest = principal * rate * time
+
+#Final Output
+print ("The simple interest is: " + str(interest))
