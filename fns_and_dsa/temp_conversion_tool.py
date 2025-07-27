de5ef924686d40ab0901d99417ae45a521f92e74
Bliss109 <cindy.ogutu@strathmore.edu>
2025-07-27 19:59:49 +0300

    Global conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,8 +3,9 @@
 # using global conversion factors to illustrate variable scope
 
 # --- Global Conversion Factors ---
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
 
 def convert_to_celsius(fahrenheit):
     """
