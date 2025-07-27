61fd34a551a1a08dd152158137c247459111d489
Bliss109 <cindy.ogutu@strathmore.edu>
2025-07-27 19:49:46 +0300

    Utilizing lists function

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,54 @@
+# Utilizing python lists to create a simple shopping list
+
+def display_menu():
+    print("\nShopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+def main():
+    shopping_list = []
+    while True:
+        display_menu
+        choice = input("Enter your choice: ").strip()
+        
+        #Adding an item
+        if choice == '1':
+            item = input("Enter item to add: ").strip()
+            if item:
+                shopping_list.append(item)
+                print(f'"{item}" added to your shopping list.')
+            else:
+                print("No item entered. Please try again.")
+              
+        #Removing an item        
+        elif choice == '2':
+            item = input("Enter item to remove: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f'"{item}" removed from your shopping list.')
+            else:
+                print(f'"{item}" not found in the list.')
+                
+        #View Shopping List
+        elif choice == '3':
+            if shopping_list:
+                print("\nYour Shopping List:")
+                for idx, item in enumerate(shopping_list, start=1):
+                    print(f"{idx}. {item}")
+                else: 
+                     print("Your shopping list is empty.")
+        
+        #Exit
+        elif choice == '4':
+            print("Goodbye! 🛒")
+            break
+        
+        else:
+            print("Invalid choice. Please try again.")
+            
+     if __name__ == "__main__":
+         main()
+                
+        
