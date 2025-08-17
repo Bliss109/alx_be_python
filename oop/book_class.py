3d35332652fbfd9f6e493442b34a6046b1503b25
Bliss109 <cindy.ogutu@strathmore.edu>
2025-08-17 21:04:06 +0300

    Constructors

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int):
+    def __init__(self, title, author, year):
         """Constructor: Initialize a Book instance with title, author, and year."""
         self.title = title
         self.author = author
@@ -9,10 +9,10 @@ class Book:
         """Destructor: Called when the Book instance is deleted."""
         print(f"Deleting {self.title}")
 
-    def __str__(self) -> str:
+    def __str__(self):
         """User-friendly string representation of the Book instance."""
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         """Official string representation of the Book instance."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
